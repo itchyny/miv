6e9cae2d7795cb93e61baac568939cbd0b7be74b
itchyny <neknekneknek.t@gmail.com>
2014-03-20 13:24:05 +0900

    improve Setting.hs: sort the plugins

--- src/Setting.hs
+++ src/Setting.hs
@@ -8,6 +8,8 @@ import Data.HashMap.Lazy hiding (map)
 import qualified Data.HashMap.Lazy as HM
 import Data.ByteString hiding (empty)
 import Data.Maybe (fromMaybe)
+import Data.List (sortBy)
+import Data.Function (on)
 
 import qualified Plugin as P
 import Config
@@ -27,9 +29,12 @@ decodeSetting = fmap toSetting . Y.decode
 
 toSetting :: SI.SettingI -> Setting
 toSetting s
-  = Setting { plugin = maybe [] (foldlWithKey' (\a k v -> P.toPlugin k v : a) []) (SI.plugin s)
+  = Setting { plugin = sortWith P.name $ maybe [] (foldlWithKey' (\a k v -> P.toPlugin k v : a) []) (SI.plugin s)
             , config = SI.config s
             , filetypeScript = maybe empty (HM.map lines) (SI.filetypeScript s)
             , beforeScript = lines $ fromMaybe "" (SI.beforeScript s)
             , afterScript = lines $ fromMaybe "" (SI.afterScript s)
   }
+
+sortWith :: Ord b => (a -> b) -> [a] -> [a]
+sortWith = sortBy . on compare
