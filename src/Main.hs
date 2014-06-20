9543f92c258b08838b2e3183ed08d0c3a4312e9f
itchyny <neknekneknek.t@gmail.com>
2014-06-20 11:00:38 +0900

    use unwords

--- src/Main.hs
+++ src/Main.hs
@@ -179,7 +179,9 @@ generateHelpTags setting = do
   forM_ (map (\p -> dir ++ P.rtpName p ++ "/doc/") (S.plugin setting))
     $ \path ->
         doesDirectoryExist path
-          >>= \exists -> when exists (void (system ("cd " ++ singleQuote path ++ " && cp * " ++ singleQuote docdir ++ " 2>/dev/null")))
+          >>= \exists -> when exists $ void
+              $ system $ unwords ["cd", singleQuote path,
+                                  "&& cp *", singleQuote docdir, "2>/dev/null"]
   _ <- system $ "vim -u NONE -i NONE -N -c 'helptags " ++ docdir ++ "' -c 'noa qa!'"
   putStrLn "Success in processing helptags."
 
