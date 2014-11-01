df571f14ecc89b6398238cbcdb6961e2e83ba393
itchyny <neknekneknek.t@gmail.com>
2014-11-01 12:54:10 +0900

    Do not use unqualified import of Data.HashMap.Lazy

--- src/Setting.hs
+++ src/Setting.hs
@@ -1,7 +1,6 @@
 module Setting where
 
 import qualified Data.Yaml as Y
-import Data.HashMap.Lazy hiding (map)
 import qualified Data.HashMap.Lazy as HM
 import Data.ByteString hiding (empty)
 import Data.Maybe (fromMaybe)
@@ -15,10 +14,10 @@ import qualified SettingI as SI
 data Setting =
      Setting { plugin         :: [P.Plugin]
              , config         :: Maybe Config
-             , filetypeScript :: HashMap String [String]
+             , filetypeScript :: HM.HashMap String [String]
              , beforeScript   :: [String]
              , afterScript    :: [String]
-             , filetypeDetect :: HashMap String String
+             , filetypeDetect :: HM.HashMap String String
      } deriving (Eq, Show)
 
 decodeSetting :: ByteString -> Maybe Setting
@@ -26,12 +25,12 @@ decodeSetting = fmap toSetting . Y.decode
 
 toSetting :: SI.SettingI -> Setting
 toSetting s
-  = Setting { plugin = sortWith P.name $ maybe [] (foldlWithKey' (\a k v -> P.toPlugin k v : a) []) (SI.plugin s)
+  = Setting { plugin = sortWith P.name $ maybe [] (HM.foldlWithKey' (\a k v -> P.toPlugin k v : a) []) (SI.plugin s)
             , config = SI.config s
-            , filetypeScript = maybe empty (HM.map lines) (SI.filetypeScript s)
+            , filetypeScript = maybe HM.empty (HM.map lines) (SI.filetypeScript s)
             , beforeScript = lines $ fromMaybe "" (SI.beforeScript s)
             , afterScript = lines $ fromMaybe "" (SI.afterScript s)
-            , filetypeDetect = fromMaybe empty (SI.filetypeDetect s)
+            , filetypeDetect = fromMaybe HM.empty (SI.filetypeDetect s)
   }
 
 sortWith :: Ord b => (a -> b) -> [a] -> [a]
