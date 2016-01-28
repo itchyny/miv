da856d20b7b5a950881e8332d059eaef596b4184
itchyny <itchyny@hatena.ne.jp>
2016-01-29 08:41:08 +0900

    improve ShowText.hs: derive instance: Show a => ShowText a

--- src/ShowText.hs
+++ src/ShowText.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE FlexibleInstances, IncoherentInstances, UndecidableInstances #-}
 module ShowText where
 
 import Data.Text
 
 class ShowText a where
   show :: a -> Text
+
+instance Show a => ShowText a where
+  show = pack . Prelude.show
