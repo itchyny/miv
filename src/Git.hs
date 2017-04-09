e2ffbf8f4a7fcc098f0e6896b6f5c8553f190873
itchyny <itchyny@hatena.ne.jp>
2017-04-10 01:47:24 +0900

    improve Git.hs: use git -C <path> instead of changing directory

--- src/Git.hs
+++ src/Git.hs
@@ -17,19 +17,19 @@ clone :: String -> FilePath -> IO ExitCode
 clone repo path = system $ unwords ["git", "clone", gitUrl repo, singleQuote path]
 
 cloneSubmodule :: String -> FilePath -> IO ExitCode
-cloneSubmodule repo path = system $ unwords ["git", "clone", gitUrl repo, singleQuote path, "&&", "cd", singleQuote path, "&&", "git", "submodule", "update", "--init", "--recursive"]
+cloneSubmodule repo path = system $ unwords ["git", "clone", gitUrl repo, singleQuote path, "&&", "&&", "git", "-C", singleQuote path, "submodule", "update", "--init", "--recursive"]
 
 pull :: FilePath -> IO ExitCode
-pull path = system $ unwords ["cd", singleQuote path, "&&", "git", "pull", "--rebase", "--prune"]
+pull path = system $ unwords ["git", "-C", singleQuote path, "pull", "--rebase", "--prune"]
 
 pullSubmodule :: FilePath -> IO ExitCode
-pullSubmodule path = system $ unwords ["cd", singleQuote path, "&&", "git", "pull", "--rebase", "--prune", "&&", "git", "submodule", "update", "--init", "--recursive"]
+pullSubmodule path = system $ unwords ["git", "-C", singleQuote path, "pull", "--rebase", "--prune", "&&", "git", "-C", singleQuote path, "submodule", "update", "--init", "--recursive"]
 
 lastUpdate :: FilePath -> IO Integer
-lastUpdate path = read <$> readProcess "sh" ["-c", unwords ["cd", singleQuote path, "&&", "git", "show", "-s", "--format=%ct", "2>/dev/null", "||", "echo", "0"]] []
+lastUpdate path = read <$> readProcess "sh" ["-c", unwords ["git", "-C", singleQuote path, "show", "-s", "--format=%ct", "2>/dev/null", "||", "echo", "0"]] []
 
 gitStatus :: FilePath -> IO ExitCode
-gitStatus path = system $ unwords ["cd", singleQuote path, ">/dev/null 2>&1", "&&", "git", "status", ">/dev/null 2>&1"]
+gitStatus path = system $ unwords ["git", "-C", singleQuote path, "status", ">/dev/null 2>&1"]
 
 gitUrl :: String -> String
 gitUrl = ("https://github.com/" <>)
